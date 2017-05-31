0de55cee41f9345fe8946fd19c3c625a1afc21d1
plopesc <plopesc@282415.no-reply.drupal.org>
2017-05-31 10:31:33 +0200

    Issue #2838443 by plopesc: Support "degrees and decimal minutes" display format - Formatter tests

--- tests/src/Kernel/GeofieldFormatterTest.php
+++ tests/src/Kernel/GeofieldFormatterTest.php
@@ -60,7 +60,7 @@ class GeofieldFormatterTest extends EntityKernelTestBase {
   /**
    * Tests geofield field default formatter.
    */
-  public function testFormatters() {
+  public function testDefaultFormatter() {
     // Create the entity to be referenced.
     $entity = EntityTest::create(['name' => $this->randomMachineName()]);
     $value = \Drupal::service('geofield.wkt_generator')->WktGenerateGeometry();
@@ -73,6 +73,130 @@ class GeofieldFormatterTest extends EntityKernelTestBase {
     $build = $entity->get('geofield')->view(['type' => 'geofield_default']);
     \Drupal::service('renderer')->renderRoot($build[0]);
     $this->assertEquals($build[0]['#markup'], $value);
+  }
+
+  /**
+   * Tests geofield field DMS formatter.
+   *
+   * @dataProvider dmsFormatterProvider
+   */
+  public function testDmsFormatter($point, $format, $expected_value) {
+    // Create the entity to be referenced.
+    $entity = EntityTest::create(['name' => $this->randomMachineName()]);
+
+    $entity->geofield = [
+      'value' => $point,
+    ];
+    $entity->save();
+
+    // Verify the geofield field formatter's render array.
+    $build = $entity->get('geofield')->view(['type' => 'geofield_dms', 'settings' => ['output_format' => $format]]);
+    \Drupal::service('renderer')->renderRoot($build[0]);
+    $this->assertEquals(trim($build[0]['#markup']), $expected_value);
+  }
+
+  /**
+   * Provides test data for testGeoConstraint().
+   */
+  public function dmsFormatterProvider() {
+    return [
+      'DMS Value' => [
+        'POINT (40 -3)',
+        'dms',
+        "<span class=\"dms dms-lat\">
+    3°
+    0'
+          0\"
+        S
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    40°
+    0'
+          0\"
+        E
+  </span>"
+      ],
+      'DM Value' => [
+        'POINT (40 -3)',
+        'dm',
+        "<span class=\"dms dms-lat\">
+    3°
+    0.00000'
+        S
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    40°
+    0.00000'
+        E
+  </span>"
+      ],
+      'DMS Value long' => [
+        'POINT (85.24587 45.625358)',
+        'dms',
+        "<span class=\"dms dms-lat\">
+    45°
+    37'
+          31\"
+        N
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    85°
+    14'
+          45\"
+        E
+  </span>"
+      ],
+      'DM Value long' => [
+        'POINT (85.24587 45.625358)',
+        'dm',
+        "<span class=\"dms dms-lat\">
+    45°
+    37.51667'
+        N
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    85°
+    14.75000'
+        E
+  </span>"
+      ],
+      'DMS Arnedo' => [
+        'POINT (-2.1021 42.2257)',
+        'dms',
+        "<span class=\"dms dms-lat\">
+    42°
+    13'
+          32\"
+        N
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    2°
+    6'
+          7\"
+        W
+  </span>"
+      ],
+      'DM Arnedo' => [
+        'POINT (-2.1021 42.2257)',
+        'dm',
+        "<span class=\"dms dms-lat\">
+    42°
+    13.53333'
+        N
+  </span>
+      ,
+    <span class=\"dms dms-lon\">
+    2°
+    6.11667'
+        W
+  </span>"
+      ],
+    ];
   }
 
 }
